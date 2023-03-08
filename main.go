71529929ac9cdec4734bee31f915a9fb9c387e32
Thomas Cousin <thomas.cousin@me.com>
2023-03-08 15:56:43 +0100

    Bug fixes

--- main.go
+++ main.go
@@ -139,7 +139,7 @@ func main() {
 	prometheus.MustRegister(pingResponseSeconds)
 
 	pingers := make([]*probing.Pinger, len(*hosts))
-	labels := make([string]string, len(*hosts))
+	labels := make([string]string)
 	var pinger *probing.Pinger
 	var host string
 	for i, host := range *hosts {
